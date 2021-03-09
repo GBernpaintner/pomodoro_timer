9a36384d4800bec36bcd71259307de4e86d3be3b
gustav <gustav@bernpaintner.com>
2021-03-10 00:35:34 +0100

    inital commit

---
+++ pomodoro.py
@@ -0,0 +1,39 @@
+import pyaudio
+import wave
+import sys
+import asyncio
+
+
+FOCUS_SOUND = 'sounds/Alesis-Sanctuary-QCard-Tines-Aahs-C4.wav'
+RELAX_SOUND = 'sounds/Alesis-Sanctuary-QCard-Tines-Aahs-C4.wav'
+FOCUS_TIME = 2
+RELAX_TIME = 2
+
+
+async def play_sound(filepath):
+    CHUNK = 1024
+    p = pyaudio.PyAudio()
+    with wave.open(filepath, 'rb') as wf:
+        stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
+        while len(data := wf.readframes(CHUNK)) > 0:
+            stream.write(data)
+        stream.stop_stream()
+        stream.close()
+
+
+async def start_pomodoro(focus_time, relax_time):
+    while True:
+        await asyncio.gather(play_sound(FOCUS_SOUND), asyncio.sleep(focus_time))
+        await asyncio.gather(play_sound(RELAX_SOUND), asyncio.sleep(relax_time))
+
+
+async def main():
+    await start_pomodoro(FOCUS_TIME, RELAX_TIME)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
